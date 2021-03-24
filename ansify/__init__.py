4e0325b9fe064ac72b9a45cfddfa0cc88f82071d
eilianxiao <eilianxiao@tencent.com>
2021-03-24 21:10:48 +0800

    🎉 initial commit

---
+++ ansify/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Awesome `ansify` is a Python CLI to create ANSI/ASCII art from images."""
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
